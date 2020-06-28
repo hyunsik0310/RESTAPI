b4ccdbfea3b546b5eab9efdb5025de400442c9da
hyunsik <hyunsik0310@gmail.com>
2020-06-29 07:18:21 +0900

    20200629 0718

--- src/main/java/com/example/restfulwebservice/user/userController.java
+++ src/main/java/com/example/restfulwebservice/user/userController.java
@@ -1,12 +1,15 @@
 package com.example.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class userController {
@@ -23,14 +26,30 @@ public class userController {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+	public User retrieveUser(@PathVariable int id) throws UserNotFoundException {
+		User user = service.findOne(id);
+		
+		if (user == null) {
+			throw new UserNotFoundException(String.format("ID[%s] not found", id));
+		}
+		
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
 		
-		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+			
+			return ResponseEntity.created(location).build();
+			
 	}
+
+
+	
+	
 }
